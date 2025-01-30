e1c2be5dc644962528a0dffe84c2250f7938d436
BelinMathis <matbelin5@gmail.com>
2025-01-30 09:35:57 +0100

    add api connection

---
+++ BACK/APILinkdinProjectV2/APILinkdinProjectV2/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using APILinkdinProjectV2.Data;
+using APILinkdinProjectV2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILinkdinProjectV2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SocialMediaContext _context;
+
+        public UsersController(SocialMediaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.User.Any(e => e.UserId == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BACK/APILinkdinProjectV2/APILinkdinProjectV2/Data/SocialMediaContext.cs
@@ -0,0 +1,40 @@
+﻿using APILinkdinProjectV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILinkdinProjectV2.Data
+{
+    public class SocialMediaContext : DbContext
+    {
+        public SocialMediaContext(DbContextOptions<SocialMediaContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; } = null!;
+        public DbSet<Post> Post { get; set; } = null!;
+        public DbSet<Comment> Comment { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Relation Post -> User (Pas de suppression en cascade)
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Posts)         
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Relation Comment -> Post (Pas de suppression en cascade)
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Relation Comment -> User (Pas de suppression en cascade)
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
